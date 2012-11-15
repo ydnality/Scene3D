5813208057cabcd3a5d85f6f9665878b0918d42d
Andy Lin <andylailin1@gmail.com>
2012-11-14 16:46:48 -0800

    added chromatic aberration

---
+++ scripts/s_s3dGenerateRadialLines.m
@@ -0,0 +1,10 @@
+%% creates ISET radial lines
+
+scene = sceneCreate('starPattern',512);
+vcAddAndSelectObject(scene); sceneWindow;
+
+% imSize = 512; spectralType = 'ep'; nLines = 8;
+% if length(varargin) >=1, imSize = varargin{1}; end
+% if length(varargin) >=2, spectralType = varargin{2}; end
+% if length(varargin) >=3, nLines = varargin{3}; end
+% scene = sceneRadialLines(scene,imSize,spectralType,nLines);
