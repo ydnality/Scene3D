d4acb6d2c73cc80f0f87bf8b5b038f218bd6cb20
Andy Lin <andylailin1@gmail.com>
2013-01-10 14:54:51 -0800

    adding pbrtHome back

---
+++ pbrtHome.m
@@ -0,0 +1,13 @@
+function [pbrtPath]=pbrtHome
+%
+%   rootPath = s3dRootPath;
+%
+% This file must reside in the root directory of the scene3D Project
+% folder.  The file is used to identify the directory of PBRT.
+
+
+%please update this to reflect the proper location of pbrt.
+% pbrtPath = '/home/andy/Dropbox/Scene3D/pbrt-v2-spectral-diffraction/';
+pbrtPath = '/home/ajwandell/Dropbox/Scene3D/pbrt-v2-spectral-diffraction/';
+return;
+
