a23fff6c07283059b22d6813b9d995a6b2028684
ydnality <ydna@hs-ln01.stanford.edu>
2013-01-31 16:37:18 -0800

    add parallel files

--- scripts/s_s3dRunParallelJob.m
+++ scripts/s_s3dRunParallelJob.m
@@ -11,16 +11,23 @@ n = 1;
 tic
 %you may change these variables to fit your job
 numProc = 128;  %number of parallel processes
-jobName = 'bunnies';
+jobName = 'bench';
 n = n + 1; %this is the job ID - increment it when adding a new job
-% pbrtFile = '/pbrtFiles/chromaticAberration.pbrt'
- %pbrtFile = '/pbrtFiles/benchScene/defaultBiggerHQP.pbrt'
+%pbrtFile = '/pbrtFiles/chromaticAberration.pbrt'
+%pbrtFile = '/pbrtFiles/benchScene/defaultBiggerHQP.pbrt'
+pbrtFile = '/pbrtFiles/benchScene/defaultBiggerCameraArray.pbrt'
 %pbrtFile = '/pbrtFiles/cones/defaultBiggerZoom.pbrt'
-pbrtFile = '/pbrtFiles/bunnyScene/bunnies.pbrt'
+%pbrtFile = '/pbrtFiles/bunnyScene/bunnies.pbrt'
 
 sgerun2(['s_s3dOneParallelJob(''' pbrtFile ''', jobindex,'  int2str(numProc) ');'],[jobName int2str(n)], 0, 1:numProc);
 toc
 'approximate time needed to set up jobs'
 
+'current time'
+c = clock;
+'hour'
+c(4)
+'minute' 
+c(5)
 %% this line is to view the Matlab output
 unix(['cat ~/sgeoutput/job_' jobName int2str(n) '.o*'])
