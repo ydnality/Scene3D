39aee780784a23f07560219737a11ba3507227a1
Andy Lin <andylailin1@gmail.com>
2013-05-16 18:17:57 -0700

    added example values to iRatioTest.m

--- compPhotography/iratio2distance.m
+++ compPhotography/iratio2distance.m
@@ -70,7 +70,6 @@ grid on
 % thus, iRatio for this point should be 4, and d1 = 1
 
 
-
 testIRatio = 4;
 f = 1;
 phi = pi/2;
@@ -100,3 +99,40 @@ d1Test = (2*f^2)/(-2*cos(alpha).*sin(phi).*f + radical)
 
 
 
+% flash 1 at origin -> intensity ~ 1/50
+% flash 2 at (0, 0, -6) -> intensity ~ 1/ 146
+% point at (3, 4, 5)
+% alpha = 0, phi = pi/2
+
+% picture 1 should have 2.92 times the radiance of picture 2
+% thus, iRatio for this point should be 2.92, and d1 = 7.0711
+
+p = [3 4 5]; d1 = 7.0711;
+testIRatio = 2.92;
+f = 6;
+alpha = asin(p(2)/d1)
+phi   = asin(p(3)/(d1*cos(alpha)))
+
+radical = sqrt(4*cos(alpha).^2*sin(phi).^2.*f.^2 - 4*f^2.*(1 - testIRatio));
+d1Test = (2*f^2)/(-2*cos(alpha).*sin(phi).*f + radical)
+
+% this checks out, d1Test = 7.0711, as expected!
+
+% flash 1 at origin -> intensity ~ 1/50
+% flash 2 at (0, 0, -6) -> intensity ~ 1/ 146
+% point at (3, 4, 5)
+% alpha = 0, phi = pi/2
+
+% picture 1 should have 2.92 times the radiance of picture 2
+% thus, iRatio for this point should be 2.92, and d1 = 7.0711
+
+p = [-3 -4 5]; d1 = 7.0711;
+testIRatio = 2.92;
+f = 6;
+alpha = asin(p(2)/d1)
+phi   = asin(p(3)/(d1*cos(alpha)))
+
+radical = sqrt(4*cos(alpha).^2*sin(phi).^2.*f.^2 - 4*f^2.*(1 - testIRatio));
+d1Test = (2*f^2)/(-2*cos(alpha).*sin(phi).*f + radical)
+
+% this checks out, d1Test = 7.0711, as expected!
