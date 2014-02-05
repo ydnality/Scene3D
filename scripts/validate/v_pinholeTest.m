1bd782655d76ab984f35bb5d3d649ba9353f37b4
Andy Lin <andylailin1@gmail.com>
2014-02-04 17:14:36 -0800

    added pinhole validation

---
+++ scripts/validate/v_pinholeTest.m
@@ -0,0 +1,30 @@
+%%  Creates a pointArray using point light sources.  
+% The point light sources are created using tiny spherical area lights 
+% diffraction is turned off in this example!
+
+%initialization and directory organization
+filePath = [datapath '/validate/pbrtObject/']
+chdir(filePath);
+mkdir('batchPbrtFiles');
+unix('rm batchPbrtFiles/*');
+unix('cp * ./batchPbrtFiles/');
+chdir('batchPbrtFiles');
+
+
+%make a new pbrt object
+clear curPbrt;
+curPbrt = pbrtObject();
+curPbrt.camera.setLens('pinhole-test.pbrt');
+curPbrt.sampler.setPixelSamples(256);  %curPbrt.sampler.setPixelSamples(131070);
+curPbrt.camera.setResolution(201, 201);
+
+%run pbrt
+tmpFileName = ['pinhole'  '.pbrt'];
+curPbrt.writeFile(tmpFileName);
+oi = s3dRenderScene(tmpFileName, 50, [filePath '/batchPbrtFiles/'], tmpFileName);
+
+chdir('..');
+%the result should be a grid of points
+
+
+
