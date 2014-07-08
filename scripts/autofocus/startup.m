1e9e16c56663fec1450707bc1fd7582752cfa88c
Brian Wandell <wandell@stanford.edu>
2014-07-07 18:31:21 -0700

    Eliminated stray startup file

--- scripts/autofocus/startup.m
+++
@@ -1,5 +0,0 @@
-chdir('/home/andy/Dropbox/ISET/');
-isetPath(pwd)
-addpath(genpath('/home/andy/Dropbox/Scene3D/Scene3DMatlab/'))
-pbrtHome = '/home/andy/Dropbox/Scene3D/pbrt-v2-spectral-diffraction/';
-chdir('/home/andy/Dropbox/Scene3D/Scene3DMatlab/autofocus/');
