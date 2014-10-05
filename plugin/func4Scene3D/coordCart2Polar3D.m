01129779e21e2da2e272bcd8409df0e61ccff1cd
Brian Wandell <wandell@stanford.edu>
2014-10-04 22:34:44 -0700

    The relationship between these functions and matlab is now commented

--- plugin/func4Scene3D/coordCart2Polar3D.m
+++ plugin/func4Scene3D/coordCart2Polar3D.m
@@ -1,37 +1,39 @@
-
-
-function  [ro,theta,zOUT]=coordCart2Polar3D(x,y,z)
-
-%Convert point coordinate from Cartesian (x,y,z) to polar in object/image plane(ro,theta,z)
+function  [rho,theta,zOUT] = coordCart2Polar3D(x,y,z)
+%Convert (x,y,z) to (rho,theta,z) 
 %
-%       function  [ro,theta,zOUT]=coordCart2Polar3D(x,y,z)
+%  [rho,theta,zOUT] = coordCart2Polar3D(x,y,z)
 %
 % INPUT:
-% x
-% y
-% z
-
+%   x, y, z
+%
 % OUTPUT:
-% ro= height eccentricity
-% theta= angular eccentricity
-% z= distance along optical axis 
+%  rho   = height eccentricity  (your units)
+%  theta = angular eccentricity (radians)
+%  z = z (distance along optical axis, usually; your units)
+%
+% Examples:
+%   x = 1, y = 1, z = 10; [rho,theta,zOUT] = coordCart2Polar3D(x,y,z)
+%   x = 0, y = 1, z = 10; [rho,theta,zOUT] = coordCart2Polar3D(x,y,z)
 %
 %  MP Vistasoft 2014
 
-
 %% HEIGHT ECCENTRICITY
-ro =sqrt(x.^2+y.^2); %distance of the point source to optical axis
+rho =sqrt(x.^2 + y.^2); %distance of the point source to optical axis
 
 %% ANGULAR ECCENTRICITY
-if not(x==0) && not(y==0)
-    theta=atan(y/x); %angle subtended by the point source and the x-axis in the object plane
-else
-    if (y==0)
-        theta=0;
-    else
-        ps_angle=pi/2;
-    end
-end
+
+theta = atan2(y,x);
+
+% Used to be this, but BW changed to atan2 after MP left.
+% if not(x==0) && not(y==0)
+%     theta=atan(y/x); %angle subtended by the point source and the x-axis in the object plane
+% else
+%     if (y==0),         theta = 0;
+%     else               theta = pi/2;
+%     end
+% end
         
 %% DISTANCE along THE OPTICAL AXIS
-zOUT=z;
+zOUT = z;
+
+end
--- plugin/func4Scene3D/coordPolar2Cart3D.m
+++ plugin/func4Scene3D/coordPolar2Cart3D.m
@@ -1,27 +1,34 @@
-
-
-function  [x,y,zOUT]=coordPolar2Cart3D(ro,theta,z)
-
-%Convert point coordinate from  polar to Cartesian (x,y,z)  in object/image plane(ro,theta,z)
-
+function  [x,y,zOUT]=coordPolar2Cart3D(rho,theta,z)
+%Convert point coordinate from  polar to Cartesian, leaving z unchanged
+%
+%  x = rho*cos(theta)
+%  y = rho*sin(theta)
+%  z = z
+%
+% This is not quite the same as pol2cart() in Matlab.  This function 
+%
 % INPUT:
-% ro= height eccentricity
-% theta= angular eccentricity
-% z= distance along optical axis 
-
+%   rho   = height eccentricity  (your units)
+%   theta = angular eccentricity in radians
+%   z = distance along optical axis (your units)
+%
 % OUTPUT:
-% x
-% y
-% z
+%  x, y, z
+%
+% Examples:
+%    rho = 10; theta = pi/4; z = 1;
+%    [x,y,z] = coordPolar2Cart3D(rho,theta,z)
+%    rho = 1;
+%    [x,y,z] = coordPolar2Cart3D(rho,theta,z)
+%
+% MP Vistasoft Copyright 2014
 
+%% This projects the (x,y) but leaves z unchanged.
 
+% The Matlab function does a cylindrical coordinate transform, which is a
+% bit different.
+x    = rho.*cos(theta);
+y    = rho.*sin(theta);
+zOUT = z;
 
-
-
-
-%% X coord
-x=ro.*cos(theta);
-%% Y coord
-y=ro.*sin(theta);
-%% Z coord
-zOUT=z;
+end
