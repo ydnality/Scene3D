07c7bc16eb47038752877c21aa0de58f811555f7
Andy Lai Lin <ydna@black>
2015-01-23 16:58:18 -0800

    added some more files

---
+++ scripts/pbrt/generateScenes/s_s3dRenderHDRBench.m
@@ -0,0 +1,10 @@
+% this scripts renders a pbrt scene straight up(using a .pbrt file), using
+% (1) a docker container, and also (2) using a pbrt executable.  In case 2,
+% pbrt must be installed on the system and be on the system path.
+
+%% Render Scene only with docker container
+dockerFlag = true;
+inputPbrtFile = fullfile(dataPath, 'pbrtScenes','benchScene', 'defaultBigger.pbrt'); 
+scene = s3dRenderScene(inputPbrtFile, 'bench', [], dockerFlag); 
+vcAddObject(scene); sceneWindow;
+
