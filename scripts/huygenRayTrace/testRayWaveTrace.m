4f169b7c375dccb98f02121ddc31f99f0a70603b
Andy <andy@Belle-Ubuntu.(none)>
2015-03-17 02:31:00 -0700

    huygens ray trace example now works for a 2D!!

--- scripts/huygenRayTrace/testRayWaveTrace.m
+++ scripts/huygenRayTrace/testRayWaveTrace.m
@@ -15,6 +15,8 @@
 %
 %
 % AL
+%%
+ieInit
 
 %% Sum the wavefronts from a series of points in a slit aperture
 
@@ -22,17 +24,18 @@
 
 % numSamples = 100000;
 lambda    = 550;                % nm
-binSize   = 20;                 % nm
-numPixels = 625;                % In the sensor
-apertureSample = 100;           % nm
-nApertureSamples = 1e3;
+binSize   = 2500   %25;                 % nm
+numPixels = 1000;                % In the sensor
+%apertureSample = 100;           % nm
+%nApertureSamples = 1e3;
 
 % We do the calculation one aperture sample at a time
 % imagePlaneLocation = [lambda * 20 0];    % This many wavelengths from the aperture
-imagePlaneDist     = lambda*20;
+imagePlaneDist     = lambda*2000;  %20
 % imagePlane         = zeros(1,numPixels); % A 1D sensor
 ph = zeros(1, numPixels);               % Phase of ray at that sample
 distance = zeros(1, numPixels);         % From aperture sample to sensor sample
+
 % intersectionPointR = zeros(1, numPixels);
 
 % imagePlanePhase = zeros(1,numPixels);   
@@ -45,8 +48,8 @@ distance = zeros(1, numPixels);         % From aperture sample to sensor sample
 %aperturePoint = (rand(1,numApertureSamples) - .5) * 2 * apertureSize/2;
 
 % Pick a set of sample points on the aperture
-numApertureSamples = 1e2;   % Number of points in the slit aperture
-apertureSize = lambda*numApertureSamples/8;         % nm
+numApertureSamples = 100;   % Number of points in the slit aperture
+apertureSize = 2500;         % nm   25
 aperturePoint = linspace(-.5,.5, numApertureSamples + 1) * apertureSize;
 
 % These are the locations on the sensor
@@ -60,138 +63,123 @@ intensity = zeros(numPixels,1);
 
 % For each aperture calculate the distance to all the pixels, keeping the
 % phase information
-for apertureSample = 1:numApertureSamples
-    
-    % These are the locations on the aperture
-    apLocations = [zeros(1,numPixels); ones(1, numPixels) * aperturePoint(apertureSample)];
-    
-    d = sqrt(diag((endLocations - apLocations)'*(endLocations - apLocations)));
-    % vcNewGraphWin; plot(d)
-    intensity = cos(2*pi*(d/lambda)) + intensity;
-end
+for rPhase = 0 %:pi/256:(255 * pi/256)
+    for apertureSample = 1:numApertureSamples
 
+        % These are the locations on the aperture
+        apLocations = [zeros(1,numPixels); ones(1, numPixels) * aperturePoint(apertureSample)];
+
+        d = sqrt(diag((endLocations - apLocations)'*(endLocations - apLocations)));
+        % vcNewGraphWin; plot(d)
+        intensity = exp(2 * pi * i * (d/lambda)) + intensity;
+    end
+end
 % Get the real intensity
 vcNewGraphWin;
-intensity  = intensity .^2;   
+intensity = abs(intensity).^2;
+
 sensorAxis = linspace(0, binSize * numPixels, numPixels);
 plot(sensorAxis, intensity);
 
-
-%%
-        % Figure out thetas given this
-        dir = [ones(1, numPixels) * imagePlaneLocation(1); endLocations] - ...
-            [zeros(1,numPixels); ones(1, numPixels) * aperturePoint(apertureSample)];
-        dir = dir ./ repmat(sqrt(sum(dir.*dir, 1)), [2 1]); %normalize
-        
-        count = 1;
-        for i = 1:numPixels
-            rayVector = dir(:, i);  %new stuff
-            distance(i) = imagePlaneLocation(1)/rayVector(1);
-
-            %phase(i) = (mod(distance(i), lambda)/lambda);
-            ph(i) = distance(i)/lambda;
-            intersectionPointR(i) = count;  %endLocations(i);
-            
-            %if ray is still within bounds, record it
-           % if(intersectionPointR(i) <= size(imagePlane,2) && intersectionPointR(i) >=1)
-                intensity(i) = cos(2*pi * ph(i) + rPhase);
-                imagePlane(intersectionPointR(i)) = imagePlane(intersectionPointR(i)) + intensity(i);
-            %end
-            count = count + 1;
-        end
-    end
-    %rPhase
-%end
-
-% theta = atan(endLocations./imagePlaneLocation(1)) * 180./pi; 
-% figure; plot(theta, endLocations);
-
-sensorAxis = linspace(0, binSize * numPixels, numPixels);
-figure; plot(sensorAxis, abs(imagePlane.^2));
-
-%% make it faster
+%% plot theoretical solution
 theta = linspace(-pi/2, pi/2, 200);
-a = 2000;
+a = 2500;
 lambda = 550;
 I = (sin(pi .* a .* sin(theta./lambda))./(pi * a .* sin(theta./lambda))).^2;
 figure; 
 plot (theta, I);
-% plot theoretical solutiongoo
 
+%% compare the 2 curves
+thetaSens = atan((sensorAxis - sensorAxis(numPixels/2))/(imagePlaneDist));
+vcNewGraphWin;
+plot(thetaSens, intensity./max(intensity(:)));
+hold on;
+plot(theta, I, 'r');
+
+title('Analytical(red) vs. Huygens(blue) raytrace');
+
+%why the error? the analytical solution uses a paraxial approximation...
+%sin(theta) ~ tan(theta) for small angles
 
 %% try in 3D instead
 
-
-%% Extra, slow
-%% A simple point source, through a single pinhole slit.  We are tracing in the forward direction in this case!  Only 2D for now
-
-%assume point source is at (-10, 0)
 %assume single pinhole is at origin
-%assume imaging plane is at (10, 0)
 
-initialIntensity = 10;
-numSamples = 100000;
-numApertureSamples = 100;
-lambda = 550;  %nm
-binSize = 128; %nm   %1/2
-apertureSize =4000; %nm
-numPixels = 625;
+% numSamples = 100000;
+wave    = 400:100:700;%550;                % nm
+binSize   = [7500 7500];   %25;                 % nm
+numPixels = [300 300];                % In the sensor
+imagePlaneDist     = 2000 * 550;  %20
 
-imagePlaneLocation = [lambda * 200 0];   %lambda * 20
-imagePlane = zeros(1,numPixels);
-imagePlanePhase = zeros(1,numPixels);
-ph = zeros(1, numSamples);
-distance = zeros(1, numSamples);
-intersectionPointR = zeros(1, numSamples);
-intersectionPoint = zeros(1, numSamples);
-intensity = zeros(1, numSamples);
-%we are using known incremental directions
-%since all the rays that make it through are assumed to go through pinhole
-%at the same phase, we can just start tracing from the pinhole
+% Pick a set of sample points on the aperture
+numApertureSamples = [50 50];   % Number of points in the slit aperture
+apertureSize = 2500;         % nm   25
+numPixelsTot = numPixels(1) * numPixels(2);
 
-aperturePoint = (rand(1,numApertureSamples) - .5) * 2 * apertureSize/2;
-aperturePoint = linspace(-.5,.5, numApertureSamples) * 2 * apertureSize/2;
+%create aperture grid
+ap1DX = linspace(-.5,.5, numApertureSamples(1) + 1) * apertureSize;
+ap1DY = linspace(-.5,.5, numApertureSamples(2) + 1) * apertureSize;
+[apXGrid apYGrid] = meshgrid(ap1DX, ap1DY);
+apXGridFlat = apXGrid(:);   %flatten to 1D for simplicity
+apYGridFlat = apYGrid(:);
+%reduce aperture grid by applying a circular aperture.  Try different
+%apertures!!
+withinAperture = (apXGridFlat.^2 + apYGridFlat.^2) < (apertureSize/2)^2;
+apXGridFlat = apXGridFlat(withinAperture);
+apYGridFlat = apYGridFlat(withinAperture);
+numApertureSamplesTot = (sum(withinAperture));
 
-rPhase = 0;
-    for apertureSample = 1:numApertureSamples
+%create sensor grid
+% These are the locations on the sensor
+endLocations1DX = linspace(-numPixels(1)/2 * binSize(1), numPixels(1)/2 * binSize(1), numPixels(1));
+endLocations1DY = linspace(-numPixels(2)/2 * binSize(2), numPixels(2)/2 * binSize(2), numPixels(2));
+[endLGridX endLGridY] = meshgrid(endLocations1DX, endLocations1DY);
+endLGridXFlat = endLGridX(:);    %flatten the meshgrid
+endLGridYFlat = endLGridY(:);
+endLGridZFlat = ones(size(endLGridYFlat)) * imagePlaneDist;
 
-        randSamples = rand(1,numSamples);
+intensity = zeros(numPixels(1), numPixels(2), length(wave));
 
-        for i = 1:numSamples
-            
-            %naive theta - shoot them at a pi/2 cone
-            theta = (randSamples(i) - .5) * pi/2;
+oi = oiCreate;
+oi = initDefaultSpectrum(oi);
+oi = oiSet(oi, 'wave', wave);
 
-            rayVector = [cos(theta) sin(theta)];
+countWave = 1;
+for lambda = wave
+    disp(lambda);
+    
+    intensityFlat = zeros(numPixels);
+    intensityFlat = intensityFlat(:);
+    for apertureSample = 1:numApertureSamplesTot
+        %disp(apertureSample)
+        % These are the locations on the aperture (we will take 1 at a time),
+        % and repmat that.
+        apLocationsX = ones(numPixelsTot, 1) * apXGridFlat(apertureSample);
+        apLocationsY = ones(numPixelsTot, 1) * apYGridFlat(apertureSample);
 
-   
-            %compute interesction point with plane, and put intensity onto the
-            %plane
-            distance(i) = imagePlaneLocation(1)/rayVector(1);
-            %distance(i) = imagePlaneLocation(1);  %spherical sensor
+        xDiff = endLGridXFlat - apLocationsX;
+        yDiff = endLGridYFlat - apLocationsY;
+        zDiff = endLGridZFlat; %aperture is assumed to be at Z = 0;
 
-            %intersectionPoint(i) = rayVector(2)*distance(i) + (apertureSample - numApertureSamples/2) * apertureSize/2 ;
+        d = sqrt(xDiff.^2 + yDiff.^2 + zDiff.^2);
 
-            intersectionPoint(i) = rayVector(2)*distance(i) + aperturePoint(apertureSample);
-            
-            %condition the intersectionPoint so it fits in the 1D image plane;
-            ph(i) = (mod(distance(i), lambda)/lambda);
-
-            intersectionPointR(i) = round(intersectionPoint(i)/binSize + size(imagePlane,2)/2);
-
-            %if ray is still within bounds, record it
-            if(intersectionPointR(i) <= size(imagePlane,2) && intersectionPointR(i) >=1)
-                intensity(i) = cos(2*pi * -ph(i) + rPhase);
-
-                %imagePlane(intersectionPointR(i)) = imagePlane(intersectionPointR(i)) + intensity(i);
-
-                imagePlane(intersectionPointR(i)) = imagePlane(intersectionPointR(i)) + intensity(i);
-            end
-            count = count + 1;
-        end
+        intensityFlat = exp(2 * pi * i .* (d/lambda)) + intensityFlat;
     end
+    
+    intensityFlat = 1/lambda .* abs(intensityFlat).^2;
+    intensity1Wave = reshape(intensityFlat, size(intensity, 1), size(intensity, 2));
+    
+    % sensorAxisX = linspace(0, binSize(1) * numPixels(1), numPixels(1));
+    % sensorAxisY = linspace(0, binSize(2) * numPixels(2), numPixels(2));
+    % 
+    % vcNewGraphWin; imagesc(intensity);
+    % vcNewGraphWin;  surf(sensorAxisX(1:2:300), sensorAxisY(1:2:300), intensity(1:2:300, 1:2:300), 'LineStyle', 'none');
+    %[sensorAxisXGrid sensorAxisYGrid] = meshgrid(sensorAxisX, sensorAxisY);
+    %plot(sensorAxis, intensity);
+    
+    intensity(:,:, countWave) = intensity1Wave;
+    countWave = countWave + 1;
+end
 
-
-sensorAxis = linspace(0, binSize * numPixels, numPixels);
-figure; plot(sensorAxis, abs(imagePlane.^2));
-
+oi = oiSet(oi, 'photons', intensity);
+vcAddObject(oi); oiWindow;
